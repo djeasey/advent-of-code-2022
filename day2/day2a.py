e9c023c0a8eb5dad9607eacc36383ed0ceb98e55
David Easey <davidjameseasey@gmail.com>
2022-12-02 23:11:52 +1100

    day 2

---
+++ day2/day2a.py
@@ -0,0 +1,32 @@
+f = open("input.txt", "r")
+lines = f.readlines()
+score = 0
+for line in lines:
+    plays = line.split(" ")
+    them = line[0]
+    you = line[2]
+    if you == "X": # Rock
+        score += 1
+        if them == "A": # Rock
+            score += 3
+        if them == "B": # Paper
+            score += 0
+        if them == "C": # Scissors
+            score += 6
+    if you == "Y": # Paper
+        score += 2
+        if them == "A": # Rock
+            score += 6
+        if them == "B": # Paper
+            score += 3
+        if them == "C": # Scissors
+            score += 0
+    if you == "Z": # Scissors
+        score += 3
+        if them == "A": # Rock
+            score += 0
+        if them == "B": # Paper
+            score += 6
+        if them == "C": # Scissors
+            score += 3
+print(score)
