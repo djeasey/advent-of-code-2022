dec47c5a776e1704cfed70789fd2142d33b2b22b
David Easey <davidjameseasey@gmail.com>
2022-12-04 16:28:17 +1100

    day 4

---
+++ day4/day4a.py
@@ -0,0 +1,12 @@
+f = open("day4/input.txt", "r")
+lines = f.readlines()
+count = 0
+for line in lines:
+    assigns = line.strip().split(",")
+    first_start = int(assigns[0].split("-")[0])
+    first_end = int(assigns[0].split("-")[1])
+    second_start = int(assigns[1].split("-")[0])
+    second_end = int(assigns[1].split("-")[1])
+    if (first_start <= second_start and second_end <= first_end) or (second_start <= first_start and first_end <= second_end):
+        count += 1
+print(count)
---
+++ day4/day4b.py
@@ -0,0 +1,10 @@
+f = open("day4/input.txt", "r")
+lines = f.readlines()
+count = 0
+for line in lines:
+    assigns = line.strip().split(",")
+    first_set = set(range(int(assigns[0].split("-")[0]),int(assigns[0].split("-")[1]) + 1))
+    second_set = set(range(int(assigns[1].split("-")[0]),int(assigns[1].split("-")[1]) + 1))
+    if first_set.intersection(second_set) != set():
+        count += 1
+print(count)
